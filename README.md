d148b641df6e6124ebe1d00695c7cd1df29f4cc2
cortezmb <michael.cortez78@gmail.com>
2020-06-10 12:13:55 -0500

    template

---
+++ README.md
@@ -0,0 +1,31 @@
+# Header 1
+
+## Header 2
+
+*regular* text. This is a **great** day.
+
+> This is a block quote
+
+1. list1
+2. list2
+3. list3
+
+
+### Header 3
+
+```python
+def myFunction():
+    pass
+```
+[A link to Markdown Guide Cheatsheet](https://www.markdownguide.org/cheat-sheet/)
+
+![alt text](https://images.unsplash.com/photo-1515879218367-8466d910aaa4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80)
+
+| Table Header 1 | Table Header 2 |
+| -------------- | -------------- |
+| info 1 | info 2 |
+| info 3 | info 4 |
+
+- list
+- list info
+- list info
--- heroRPG.py
+++ heroRPG.py
@@ -13,44 +13,59 @@ class Character:
         self.power = power
 
     def alive(self):
-        # Since I am in the Hero class, I use self.health
         if self.health > 0:
             return True
         else: 
             return False
 
+    def attack(self, enemy):
+        # Hero attacks goblin
+        enemy.health -= self.power
+        if self.character_name == "hero":
+            print(f"You do {self.power} damage to the {enemy.character_name}.")
+        if enemy.health <= 0:
+            print(f"The {enemy.character_name} is dead.")
+        if self.character_name == "goblin":
+            print(f"The {self.character_name} does {self.power} damage to you.")      
+        return main()
+
 # Hero class that stores health and power
 class Hero(Character):
-
-    def attack(self):
-        # Hero attacks goblin
-        goblin.health -= hero.power
-        print("You do {} damage to the goblin.".format(hero.power))
-        if goblin.health <= 0:
-            print("The goblin is dead.")
-        else:
-            return main()
+    def __init__(self, health, power):
+        self.character_name = "hero"
+        #Why and when is this needed?
+        super(Hero, self).__init__(health, power)
+    # def attack(self):
+    #     # Hero attacks goblin
+    #     goblin.health -= hero.power
+    #     print("You do {} damage to the goblin.".format(hero.power))
+    #     if goblin.health <= 0:
+    #         print("The goblin is dead.")
+    #     else:
+    #         return main()
 
     def print_status(self):
         if self.alive():
-            print("You have {} health and {} power.".format(hero.health, hero.power))
+            print(f"You have {hero.health} health and {hero.power} power.")
         else:
             return main()
 
 class Goblin(Character):
-
-    def attack(self):
-        # Goblin attacking Hero
-        hero.health -= goblin.power
-        print("The goblin does {} damage to you.".format(goblin.power))
-        if hero.health <= 0:
-            print("You are dead.")
-        else:
-            return main()
+    def __init__(self, health, power):
+        self.character_name = "goblin"
+        super(Goblin, self).__init__(health, power)
+    # def attack(self):
+    #     # Goblin attacking Hero
+    #     hero.health -= goblin.power
+    #     print("The goblin does {} damage to you.".format(goblin.power))
+    #     if hero.health <= 0:
+    #         print("You are dead.")
+    #     else:
+    #         return main()
 
     def print_status(self):
         if self.alive():
-           print("The goblin has {} health and {} power.".format(goblin.health, goblin.power)) 
+           print(f"The goblin has {goblin.health} health and {goblin.power} power.") 
         else:
             return main()
 
@@ -71,20 +86,19 @@ def main():
         raw_input = input()
         if raw_input == "1":
             # Hero attacks goblin
-            return hero.attack()
-
+            return hero.attack(goblin)
         elif raw_input == "2":
-            pass
+            return goblin.attack(hero)
+            # pass
         elif raw_input == "3":
             print("Goodbye.")
             break
         else:
             print("Invalid input {}".format(raw_input))
-
-        if goblin.alive():
-            # Goblin attacks hero
-            return goblin.attack()
-        else:
+        # if goblin.alive():
+        #     # Goblin attacks hero
+        #     return goblin.attack(hero)
+        # else:
             return False
 
 main()
