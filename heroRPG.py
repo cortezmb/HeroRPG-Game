354fb97b2657b2a0e9f7a6092ca209420d821a00
cortezmb <michael.cortez78@gmail.com>
2020-06-11 17:55:43 -0500

    Made the hero generate double damage points during an attack with a probability of 20%

--- heroRPG.py
+++ heroRPG.py
@@ -5,6 +5,7 @@
 # 1. fight Goblin
 # 2. do nothing - in which case the Goblin will attack him anyway
 # 3. flee
+import random
 
 # Character class that will be the parent of Hero and Goblin
 class Character:
@@ -20,31 +21,40 @@ class Character:
 
     def attack(self, enemy):
         # Hero attacks Goblin
-        if enemy.character_name != "zombie":
-            enemy.health -= self.power
-
+        enemy.health -= self.power
+        chance_double = random.randint(1, 10)
         if self.character_name == "hero":
-            print(f"You do {self.power} damage to the {enemy.character_name}.")
-
-        if enemy.health <= 0:
-            print(f"The {enemy.character_name} is dead.")
+            if chance_double == 2:
+                self.power = self.power * 2  
+                print(f"You do {self.power} damage to the {enemy.character_name}.")
+                return main()
+            else:
+                self.power !=  self.power * 2
+                print(f"You do {self.power} damage to the {enemy.character_name}.")                       
+            if enemy.health <= 0:
+                print(f"The {enemy.character_name} is dead.")
+            return main() 
 
         #Goblin attacks Hero
         if self.character_name == "goblin":
             print(f"The {self.character_name} does {self.power} damage to you.")
-
-        if self.character_name == "zombie":
-            self.health += hero.power
-            print(f"The {self.character_name} does {self.power} damage to you.")             
-        return main()
+        #    return main()
 
     def print_status(self, enemy):
+      
+        #Health and power status of Hero
         if self.character_name == "hero":
             print(f"You have {self.health} health and {self.power} power.")
-        elif self.character_name == "goblin":
-            print(f"The {enemy.character_name} has {enemy.health} health and {enemy.power} power.") 
-        if self.character_name == "zombie":
-            print(f"The {self.character_name} has {self.health} health and {self.power}")
+
+         #Health and power status of Goblin
+        chance_double = random.randint(1, 10)
+        if self.character_name == "goblin":
+            if chance_double == 2:
+                enemy.health = enemy.health / 2     
+                print(f"The {enemy.character_name} has {enemy.health} health and {enemy.power} power.")
+            else:
+                enemy.health != enemy.health / 2
+                print(f"The {enemy.character_name} has {enemy.health} health and {enemy.power} power.")
 
 # Hero class that stores health and power
 class Hero(Character):
@@ -59,28 +69,28 @@ class Goblin(Character):
         super(Goblin, self).__init__(health, power)
         self.alive_name = "goblin"
 
-class Zombie(Character):
+class Medic(Character):
     def __init__(self, health, power):
-        self.character_name = "zombie"
         # Manually call the constructor in base class. Then pass in health and power. 
-        super(Zombie, self).__init__(health, power)
+        self.character_name = "medic"
+        super(Medic, self).__init__(health, power)
 
-hero = Hero(10, 5)
-goblin = Goblin(6, 2)
-zombie = Zombie(10, 1)
+
+hero = Hero(10, 1)
+goblin = Goblin(10, 1)
+medic = Medic(10, 5)
 
 def main():
 
-    while goblin.alive() and hero.alive() and zombie.alive():
+    while goblin.alive() and hero.alive():
         hero.print_status(hero) 
         goblin.print_status(goblin)
-        zombie.print_status(zombie)
+        medic.print_status(goblin)
         print()
         print("What do you want to do?")
         print("1. fight goblin")
         print("2. do nothing")
-        print("3. have zombie attack")
-        print("4. flee")
+        print("3. flee")
         print("> ", end='')
         raw_input = input()
         if raw_input == "1":
@@ -90,9 +100,6 @@ def main():
             #Goblin attacks Hero
             return goblin.attack(hero)
         elif raw_input == "3":
-            #Zombie attack Hero
-            return zombie.attack(hero)
-        elif raw_input == "4":
             print("Goodbye.")
             break
         else:
